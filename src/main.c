b8777078afdcd683481e65578b98caf2973fd548
0xKeeper <abysskeeper@protonmail.com>
2024-05-06 23:27:13 +0330

    added: command line interface

--- src/main.c
+++ src/main.c
@@ -1,6 +1,40 @@
+#include <stdio.h>
 #include "../lib/libserial/libserial.h"
 
 int main(int argc, char *argv[])
 {
-    return 0;
+    int fd;
+    int baudrate; 
+    char *ptr;
+
+    if (argc < 2)
+    {
+        return EXIT_FAILURE;
+    }
+    else if (argc < 3)
+    {
+        return EXIT_FAILURE;
+    }
+    else if (validateSerialPort(argv[1]) < 0)
+    {
+        return EXIT_FAILURE;
+    }
+    else if (validateBaudRate(argv[2]) < 0)
+    {
+        return EXIT_FAILURE;
+    }
+    baudrate = atoi(argv[2]);
+
+    if ((fd = openSerialPort(argv[1], baudrate, 02)) < 0)
+    {
+        return EXIT_FAILURE;
+    }
+
+    do
+    {
+        ptr = readSerialPort(fd);
+        printf("%s", ptr);
+    } while (ptr != NULL);
+    
+    return EXIT_SUCCESS;
 }
